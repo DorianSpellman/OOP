11aae73c977c90c42d83f688a2f8e373d5f5b801
Leonard Milevsky <leomilevsky@gmail.com>
2022-06-27 17:56:32 +0300

    master

--- ex1.py
+++ ex1.py
@@ -6,15 +6,18 @@ a = Point()
 b = Point()
 
 a.color, a.circle = 'blue', 4 # local attribute of the object 'a'
-print(a.__dict__)
-print(b.__dict__) # empty collection
+print(a.__dict__) # {'color': 'blue', 'circle': 4}
+print(b.__dict__) # {} empty local collection
 
-Point.line = 'bold'
+# create new attribute. WAY 1
+Point.line = 'bold' 
 print(Point.__dict__)
 
+# create new attribute. WAY 2
 setattr(Point, 'line', 'thin')
 print(Point.__dict__)
 
+# output with correct error if it will appear
 print(getattr(Point, 'circle', 'Not found'))
 print(getattr(Point, 'type', 'Not found'))
 
@@ -25,5 +28,10 @@ print(hasattr(Point, 'line')) # False
 
 delattr(a, 'circle') # delete LOCAL property 'circle' of the object 'a'
 print(a.circle) # property 'circle' get value from Point
+
+#create LOCAL property of object
+a.x, a.y = 1, 1
+b.x, b.y = 4, 4
+print(a.__dict__, b.__dict__, sep='\n')
 
 
