c7a4d8d03c2d7536426dd61677ab71e2d2933605
Leonard Milevsky <leomilevsky@gmail.com>
2022-08-18 09:45:25 +0300

    finished

--- ex17.py
+++ ex17.py
@@ -373,3 +373,164 @@ print(mt[0, 0]) # 1
 print(mt[0, 1]) # 2
 print(mt[1, 0]) # 3
 print(mt[1, 1]) # 4
+
+print('*************************************************')
+
+from random import randint
+
+class TicTacToe:
+
+    FREE_CELL = 0      # свободная клетка
+    HUMAN_X = 1        # крестик (игрок - человек)
+    COMPUTER_O = 2     # нолик (игрок - компьютер)
+
+    def __init__(self):
+        self.size = 3
+        self.win = 0 # 0 - игра; 1 - победа чел; 3 - победа комп; 4 - ничья
+        self.pole = tuple(tuple(Cell() for _ in range(self.size)) for _ in range(self.size))
+
+    def init(self):
+        for row in self.pole:
+            for cell in row:
+                cell.is_free = True
+                cell.value = 0
+
+        self.win = 0
+
+    def check(self, indx):
+        if type(indx) not in (tuple, list) or len(indx) != 2:
+            raise IndexError('некорректно указанные индексы')
+
+        i, j = indx
+        if not (0 <= i < self.size) or not (0 <= j < self.size):
+            raise IndexError('некорректно указанные индексы') 
+
+    def update_win_status(self):
+        # проверка по строкам
+        for row in self.pole:
+            if all(x.value == self.HUMAN_X for x in row):
+                self.win = 1
+                return
+            if all(x.value == self.COMPUTER_O for x in row):
+                self.win = 2
+                return
+
+        # проверка по столбцам
+        for i in range(self.size): 
+            if all(x.value == self.HUMAN_X for x in (row[i] for row in self.pole)):
+                self.win = 1
+                return
+            if all(x.value == self.COMPUTER_O for x in (row[i] for row in self.pole)):
+                self.win = 2
+                return  
+
+        # проверка по диагоналям
+        if all(self.pole[i][i].value == self.HUMAN_X for i in range(self.size)) or \
+            all(self.pole[i][-1-i].value == self.HUMAN_X for i in range(self.size)):
+            self.win = 1
+            return
+
+        if all(self.pole[i][i].value == self.COMPUTER_O for i in range(self.size)) or \
+            all(self.pole[i][-1-i].value == self.COMPUTER_O for i in range(self.size)):
+            self.win = 2
+            return
+
+        # проверка на ничью
+        if all(x.value != self.FREE_CELL for row in self.pole for x in row):
+            self.win = 3
+         
+    def __setitem__(self, indx, value):
+        self.check(indx)
+        i, j = indx
+        self.pole[i][j].value = value
+
+        self.update_win_status()
+        
+    def __getitem__(self, indx):
+        self.check(indx)
+        i, j = indx
+        return self.pole[i][j].value
+
+    # отображение текущего состояния игрового поля (как именно - на свое усмотрение)
+    def show(self):
+        for row in self.pole:
+            print(*map(lambda x: '#' if x.value == 0 else x.value, row))
+        print()
+
+    # реализация хода игрока (запрашивает координаты свободной клетки и ставит туда крестик)
+    def human_go(self):
+        if not self:
+            return
+
+        while True:
+            i, j = map(int, input('Введите координаты клетки: ').split())
+            if not (0 <= i < self.size) or not (0 <= j < self.size):
+                continue
+            if self[i, j] == self.FREE_CELL:
+                self[i, j] = self.HUMAN_X
+                break
+    
+    # реализация хода компьютера (ставит случайным образом нолик в свободную клетку)
+    def computer_go(self): 
+        if not self:       
+            return
+
+        while True:
+            i = randint(0, self.size-1)
+            j = randint(0, self.size-1)
+            if self[i, j] != self.FREE_CELL:
+                continue
+            self[i, j] = self.COMPUTER_O
+            break
+
+
+    @property # возвращает True, если победил человек
+    def is_human_win(self):
+        return self.win == 1
+
+    @property #  возвращает True, если победил компьютер
+    def is_computer_win (self):
+        return self.win == 2
+
+    @property # возвращает True, если ничья
+    def is_draw (self):
+        return self.win == 3
+
+    # возвращает True, если игра не окончена 
+    # (никто не победил и есть свободные клетки) 
+    def __bool__(self):
+        return self.win == 0 and self.win not in (1, 2, 3)
+
+class Cell:
+
+    def __init__(self):
+        self.value = 0
+
+    def __bool__(self):
+        return self.value == 0
+
+
+
+game = TicTacToe()
+game.init()
+step_game = 0
+
+while game:
+    game.show()
+
+    if step_game % 2 == 0:
+        game.human_go()
+    else:
+        game.computer_go()
+
+    step_game += 1
+
+
+game.show()
+
+if game.is_human_win:
+    print("Поздравляем! Вы победили!")
+elif game.is_computer_win:
+    print("Все получится, со временем")
+else:
+    print("Ничья.")
